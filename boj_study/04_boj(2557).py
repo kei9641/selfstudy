d158f826bc0378e8ec91e0167517c54f05549d3a
ssafykyj <kei9641@naver.com>
2020-01-28 18:28:54 +0900

    200128 | boj 1~20

---
+++ boj_study/04_boj(2557).py
@@ -0,0 +1,13 @@
+# 2557번
+
+'''
+Hello World
+Hello World!를 출력하시오.
+
+Hello World!를 출력하시오.
+
+예제 출력)
+Hello World!
+'''
+
+print('Hello World!')
---
+++ boj_study/20_boj(8393).py
@@ -0,0 +1,10 @@
+'''
+n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+'''
+
+n = int(input())
+result = 0
+
+for num in range(1, n+1):
+    result += num
+print(result)
