482f44b40cf2bac037bd262f3e3c57c4049c25ec
kei9641 <kei9641@naver.com>
2020-04-04 18:50:28 +0900

    200404

--- boj_study/18511_boj_re.py
+++ boj_study/18511_boj_re.py
@@ -1,25 +1,7 @@
 N, K = input().split()
-k = int(K)
+K = int(K)
 set_K = list(input().split())
 result = ''
 
-if k == 1:
-    for i in range(len(N)-1, -1, -1):
-        if N[i] < set_K[0]:
-            result += set_K[0] * (len(N)-(i+1))
-            break
-    else:
-        result += set_K[0] * (len(N)-1)
-
-while i < len(N):
-    for j in range(len(set_K)-1, -1, -1):
-        if N[i] == set_K[j]:
-            result[i] = set_K[j]
-            i += 1
-            break
-        elif N[i] > set_K[j]:
-            result += set_K[j]
-            i = len(N)
-            break
-
-print(result)
+print(set_K[-K+1])
+# print(result)
